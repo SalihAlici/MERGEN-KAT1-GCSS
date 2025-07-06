0e337393bd1020f4341f534a0ea44d034e060d77
salih <salihalici987@gmail.com>
2025-07-06 19:49:19 +0300

    aras açıklama eklendi

--- MERGEN-KAT1-GCSS/Aras.cs
+++ MERGEN-KAT1-GCSS/Aras.cs
@@ -12,40 +12,43 @@ namespace MERGEN_KAT1_GCSS
 {
     public class Aras
     {
-
+        // PictureBox dizisi hata lambaları için tutulur
         private PictureBox[] pictureBoxes;
 
+        // PictureBox'ları parametre olarak alır ve saklar
         public Aras(params PictureBox[] pbs)
         {
             pictureBoxes = pbs;
         }
 
+        // Hata durumunu güncelleyen metot
         public void Update(string error)
         {
+            // Hata stringinin uzunluğu kontrol edilir 6 karakter olmalı
             if (error.Length != 6)
                 throw new ArgumentException("error stringi sayısı eşleşmiyor!");
 
-            bool alarm = false;
+            bool alarm = false; // Alarm durumu için bayrak başlangıçta false
 
+            // Her PictureBox için hata durumunu kontrol et
             for (int i = 0; i < pictureBoxes.Length; i++)
             {
-                if (error[i] == '0')
+                if (error[i] == '0') // Hata yoksa 0
                 {
-                    pictureBoxes[i].BackColor = Color.Chartreuse; 
+                    pictureBoxes[i].BackColor = Color.Chartreuse; // Yeşil renk 
                 }
-                else if (error[i] == '1')
+                else if (error[i] == '1') // Hata varsa 1
                 {
-                    pictureBoxes[i].BackColor = Color.Red;
-                    alarm = true;
+                    pictureBoxes[i].BackColor = Color.Red; // Kırmızı renk 
+                    alarm = true; // Alarm durumu aktif
                 }
             }
 
+            // Eğer alarm durumu oluşmuşsa uyarı sesi çal
             if (alarm)
             {
                 SystemSounds.Exclamation.Play();
             }
         }
-
-
     }
 }
